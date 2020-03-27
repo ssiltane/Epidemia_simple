9eb78660f96c9c76feb7a1f462e8a622af0e8f41
Siltanen <smsiltan@LM8-516-005.local>
2020-03-27 14:38:16 +0200

    Finnish version written. / Koodi suomeksi.

--- SimppeliEpidemiaMalli.m
+++ SimppeliEpidemiaMalli.m
@@ -1,84 +1,97 @@
-% Simple epidemic model with no differential equations
+% Yksinkertainen malli tarttuvan taudin leviämiselle. Tämä ei perustu
+% differentiaaliyhtälöihin vaan todennäköisyyspohjaisiin siirtymisiin
+% kategoriasta toiseen. Pohjalan on SIR-malli, jossa jokainen henkilö 
+% kuuluu yhteen seuraavista kategorioista: 
+% S sairaudelle alttiit (Susceptile)
+% I infektoituneet (Infected)
+% R rentoutuneet (taudin sairastaneet ja immuuniksi muuttuneet) (Recovered)
+% 
+% Mallin taustalla on klassinen SIR-malli, katso esimerkiksi 
+% https://www.maa.org/press/periodicals/loci/joma/the-sir-model-for-spread-of-disease-the-differential-equation-model
 %
-% Samuli Siltanen March 2020
+% Samuli Siltanen maaliskuu 2020
 
-%% Preliminaries
+%% Valmisteluja
 
 
-% Number of epidemics
+% Kuinka monta epidemiaa mallinnetaan
 Nepidemics = 100;
 
-% Graphical parameters
+% Graafisia parametreja
 fsize = 30;
 lwidth = 3;
 
-% Number of people in the village
+% Ihmisten määrä mallissa
 N = 10000;
 
-% Number of days in the simulation
+% Kuinka monta päivää simulaatiota ajetaan
 Nt = 100;
 
-% Number of people one person meets in a day on average
+% Montako muuta ihmistä yksi henkilö tapaa päivässä keskimäärin
 Nmeet = 20;
 
-% Number of days needed for recovery
+% Montako päivää kestää parantua taudista
 Nrecov = 5;
 
-% Probability of infection
+% Sairaudelle alttiin henkilön todennäköisyys saada tartunta, 
+% kun on kohdannut infektoituneen henkilön
 Pinf = .5;
 
-% How many Intensive Care Unit beds are there
+% Montako sairaalasänkyä on käytössä
 Nbeds = round(N/3);
 Nbeds = 2500;
 
-% Create plot window
+% Perusta ikkuna kuvaa varten
 figure(1)
 clf
+
+% Piirrä perustilanne kuvaan 
 plot([1,Nt],[Nbeds,Nbeds],'k','linewidth',2)
-%t1 = text(Nt/2,Nbeds+1,'Number of ICU beds');
 t1 = text(.35*Nt,1.1*Nbeds,'Sairaalapaikkojen määrä');
 set(t1,'fontsize',fsize)
 axis([1 Nt 0 N])
-%xlabel('Time (days)','fontsize',fsize)
 xlabel('Aika (päiviä)','fontsize',fsize)
-%ylabel('Number of infected people','fontsize',fsize)
 ylabel('Tartunnan saaneiden määrä','fontsize',fsize)
 set(gca,'ytick',[1000:1000:N],'fontsize',fsize)
 hold on
 
-%% Loop over epidemics
+%% For-luuppi epidemioiden yli
 for ppp = 1:Nepidemics
     
-    % Initialize matrix for recording status of people. The first row tells the
-    % number of Infected people and the second row the number of Recovered.
-    % At all times it holds that S = N-(I+R).
+    % Alustetaan tilannematriisi, joka kuvaa epidemian kulun päivä päivältä. 
+    % Jokaiselle päivälle on oma sarakkeensa. 
+    % Ensimmäinen rivi kertoo infektoituneiden ihmisten määrän I
+    % Toinen rivi kertoo rentoutuneiden ihmisten määrän R
+    % Jokaisena päivänä S = N-(I+R).
     SIRmat = zeros(2,Nt);
     
-    % Status vector of people. Here
+    % N-ulotteinen vektori, jossa on ihmisten tilanteet listattuna
     % 1 = Susceptible
     % 2 = Infected
     % 3 = Recovered
     peoplevec = ones(1,N);
     
-    % On the first day there is one infected person
+    % Ensimmäisenä päivänä on yksi sairastunut
     peoplevec(randi(N)) = 2;
     SIRmat(1,1) = 1;
     
-    % Loop over days, starting from Day 2
+    % For-luuppi päivien yli, alkaen päivästä 2
     for iii = 2:Nt
         
-        % New infections. Pick out people randomly from the population and
-        % infect half of the susceptibles who met an infected person.
-        peoplevec = peoplevec(randperm(length(peoplevec))); % Random shuffle
-        N_met_sick = Nmeet*SIRmat(1,iii-1); % This many people met a sick person
+        % Uudet infektiot. 
+        % Poimitaan satunnaisesti ihmiset, jotka ovat kohdanneet sairastuneen.        
+        % Otetaan heistä esiin sairaudelle alttiit; tästä joukosta
+        % sairastuu osa todennäköisyyden Pinf mukaisesti
+        peoplevec = peoplevec(randperm(length(peoplevec))); % Satunnainen sekoitus
+        N_met_sick = Nmeet*SIRmat(1,iii-1); % Näin moni kohtasi sairastuneen
         for jjj = 1:min(N_met_sick,N)
             if (peoplevec(jjj)==1)&(rand<Pinf)
                 peoplevec(jjj) = 2;
             end
         end
         
-        % Recoveries: infected people move to "recovered" category
-        if iii>Nrecov % Recoveries start after Nrecov days
+        % Rentoituneiden luominen eli sairaudesta paraneminen. 
+        if iii>Nrecov % Paraneminen alkaa Nrecov päivän jälkeen
             for jjj = 1:N
                 if (peoplevec(jjj)==2)&(rand<(1/Nrecov))
                     peoplevec(jjj) = 3;
@@ -86,17 +99,14 @@ for ppp = 1:Nepidemics
             end
         end
         
-        % Update situation matrix
+        % Päivitä tilannematriisi
         SIRmat(1,iii) = length(find(peoplevec==2));
         SIRmat(2,iii) = length(find(peoplevec==3));
     end
     
-    % Plot the result
+    % Piirrä kuva
     figure(1)
     plot([1:Nt],SIRmat(1,:),'r','linewidth',lwidth)
-    %plot([1:Nt],SIRmat(1,:),'r.')
-    % plot([1:Nt],SIRmat(2,:),'b')
-    % plot([1:Nt],SIRmat(2,:),'b.')
     axis([1 Nt 0 N])
     t2 = text(.1*Nt,17/25*N,[num2str(Nmeet),' kohtaamista päivässä']);
     set(t2,'fontsize',fsize,'color',[1 0 0])
@@ -105,43 +115,30 @@ for ppp = 1:Nepidemics
 end
 
 
-%% Let's compare the above to the case of social distancing
-% Number of people one person meets in a day on average
+%% Lasketaan, mitä tapahtuu, kun vähennämme ihmisten välisiä kohtaamisia
+
+% Muuten lasku on täysin sama kuin yllä; siksi jätän kommentit pois. 
+
+% Montako muuta ihmistä yksi henkilö tapaa päivässä keskimäärin
 Nmeet = 1;
 
-% Loop over epidemics
 for ppp = 1:Nepidemics
     
-    % Initialize matrix for recording status of people. The first row tells the
-    % number of Infected people and the second row the number of Recovered.
-    % At all times it holds that S = N-(I+R).
     SIRmat = zeros(2,Nt);
-    
-    % Status vector of people. Here
-    % 1 = Susceptible
-    % 2 = Infected
-    % 3 = Recovered
     peoplevec = ones(1,N);
-    
-    % On the first day there is one infected person
     peoplevec(randi(N)) = 2;
     SIRmat(1,1) = 1;
     
-    % Loop over days, starting from Day 2
     for iii = 2:Nt
-        
-        % New infections. Pick out people randomly from the population and
-        % infect half of the susceptibles who met an infected person.
-        peoplevec = peoplevec(randperm(length(peoplevec))); % Random shuffle
-        N_met_sick = Nmeet*SIRmat(1,iii-1); % This many people met a sick person
+        peoplevec = peoplevec(randperm(length(peoplevec)));
+        N_met_sick = Nmeet*SIRmat(1,iii-1); 
         for jjj = 1:min(N_met_sick,N)
             if (peoplevec(jjj)==1)&(rand<Pinf)
                 peoplevec(jjj) = 2;
             end
         end
         
-        % Recoveries: infected people move to "recovered" category
-        if iii>Nrecov % Recoveries start after Nrecov days
+        if iii>Nrecov 
             for jjj = 1:N
                 if (peoplevec(jjj)==2)&(rand<(1/Nrecov))
                     peoplevec(jjj) = 3;
@@ -149,12 +146,11 @@ for ppp = 1:Nepidemics
             end
         end
         
-        % Update situation matrix
         SIRmat(1,iii) = length(find(peoplevec==2));
         SIRmat(2,iii) = length(find(peoplevec==3));
     end
     
-    % Plot the result
+    % Piirrä kuva
     figure(1)
     plot([1:Nt],SIRmat(1,:),'b','linewidth',lwidth)
     axis([1 Nt 0 N])
